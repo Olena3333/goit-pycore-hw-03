68fc7e873bcd932b776227e63d8f73db323e23d2
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2025-10-15 21:09:07 +0200

    get_days_from_today

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ print(get_days_from_today('2025-12-12'))
 print(get_days_from_today('09-10-2020'))  
 print(get_days_from_today('2020/10/09'))  
 print(get_days_from_today('2020-13-01'))  
+
+import random
+
+def get_numbers_ticket(min_value, max_value, quantity):
+
+    if type(min_value) != int or type(max_value) != int or type(quantity) != int:
+        return []
+
+    if min_value < 1 or max_value > 1000:
+        return []
+    
+    if quantity < 1 or quantity > max_value - min_value + 1:
+        return []
+    
+    numers = random.sample(range(min_value, max_value + 1), quantity)
+    return sorted(numers)
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
